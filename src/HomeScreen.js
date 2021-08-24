f84ff259b84d211928d9709573581e1e597b2281
adalberto_parada <rafa.adalberto@gmail.com>
2021-08-24 16:52:34 -0600

    Practicando context

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,9 +1,13 @@
-import React from 'react'
-
+import React,{useContext} from 'react'
+import UserContext from './UserContext'
 const HomeScreen = () => {
+    const userContext = useContext(UserContext);
     return (
         <div>
             <h1>HomeScreen</h1>
+            <pre>
+                {JSON.stringify(userContext)}
+            </pre>
         </div>
     )
 }
--- src/MainApp.js
+++ src/MainApp.js
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import AppRouter from "./AppRouter"
 import UserContext from "./UserContext";
 const MainApp = () => {
-    const user={id:1,name:'Rafa',email:'rafa.adalberto@gmail.com'};
+    const [user,setUser] = useState({});
     return (
         <UserContext.Provider value={user}>
             <AppRouter />
